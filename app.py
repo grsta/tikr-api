165db45ec5745359f0ce804053e937092be92673
grsta <Ulrichmanor@gmail.com>
2025-06-18 20:55:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,13 +86,12 @@ def get_locker():
             data = json.load(f)
         return jsonify(data)
         
- @app.route("/knowledge", methods=["GET"])
- def get_knowledge():
+@app.route("/knowledge", methods=["GET"])
+def get_knowledge():
     try:
         with open("knowledge.json") as f:
             data = json.load(f)
         return jsonify(data)
-        
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
